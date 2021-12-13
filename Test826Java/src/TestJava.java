a37947296ad088f062d579bb733e538ac7bea9c3
Y-Rim <qwer2668@naver.com>
2021-12-13 19:24:48 +0900

    커밋 5.0

--- Test826Java/src/TestJava.java
+++ Test826Java/src/TestJava.java
@@ -2,7 +2,8 @@
 public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT World!");
-		System.out.println("Version : 1.0");
+		System.out.println("Version : 5.0");
+		System.out.println("인코딩 변경 및 로그인 기능 개선");
 	}
 
 }
